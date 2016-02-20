330f88272eb1b3e8351f8068c20dfd4c858c267a
ze <3101135386@qq.com>
2016-02-20 20:42:04 +0800

    gai

---
+++ WebApplication3/Data Access Layer/database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication3.Data_Access_Layer
+{
+    public class database:DbContext
+    {
+    }
+}
